50ec90bf7994b8640436c0fc53f11ec2c009e2c5
I2k3 <itzel5kari10@gmail.com>
2023-02-27 13:29:48 -0600

    branch base code

---
+++ motorcycles/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here 
+
+class Motocicleta(models.Model):
+
+    description = models.TextField(blank=True)
+    marca = models.TextField(default='',blank=False)
+    modelo = models.TextField(default='',blank=False)
+    motor = models.TextField(default='',blank=False)
+    consumo_g= models.PositiveSmallIntegerField(default='0',blank=False)
+    capacidad_g= models.FloatField(default='',blank=False)
+    cilindraje= models.IntegerField(default='',blank=False)
+    version= models.TextField(default='',blank=False)
+    color= models.TextField(default='',blank=False)
+    precio= models.DecimalField(default=0, max_digits=10, decimal_places=2, blank =False)
