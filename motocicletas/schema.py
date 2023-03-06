9e868326f1fe5196654d8453ae5e8402961ecd86
I2k3 <itzel5kari10@gmail.com>
2023-03-06 13:56:00 -0600

    add mutation #2

--- motocicletas/schema.py
+++ motocicletas/schema.py
@@ -6,5 +6,8 @@ import motorcycles.schema
 class Query(motorcycles.schema.Query, graphene.ObjectType):
     pass
 
+class Mutation(motorcycles.schema.Mutation, graphene.ObjectType):
+    pass
 
 schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
