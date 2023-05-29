45e0bdc131a626663a5a4d4ea156acf4b4ef3510
I2k3 <itzel5kari10@gmail.com>
2023-05-29 14:13:10 -0600

    add filtering

--- motorcycles/models.py
+++ motorcycles/models.py
@@ -1,4 +1,5 @@
-
+from django.conf import settings
+# ...code
 from django.db import models
 
 # Create your models here 
@@ -15,3 +16,10 @@ class Motocicleta(models.Model):
     version= models.TextField(default='',blank=False)
     color= models.TextField(default='',blank=False)
     precio= models.DecimalField(default=0, max_digits=10, decimal_places=2, blank =False)
+
+    posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+
+
+class Vote(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    motocicleta = models.ForeignKey('motorcycles.Motocicleta', related_name='motocicleta', on_delete=models.CASCADE)
--- motorcycles/schema.py
+++ motorcycles/schema.py
@@ -1,19 +1,45 @@
 import graphene
 from graphene_django import DjangoObjectType
-
+from users.schema import UserType
 from .models import Motocicleta
+from motorcycles.models import Motocicleta, Vote
+from graphql import GraphQLError
+from django.db.models import Q
+
+
+
 
 
 class MotocicletaType(DjangoObjectType):
     class Meta:
         model = Motocicleta
+# ...code
+# Add after the LinkType
+class VoteType(DjangoObjectType):
+    class Meta:
+        model = Vote
 
 
 class Query(graphene.ObjectType):
-    motorcycles = graphene.List(MotocicletaType)
+    motorcycles = graphene.List(MotocicletaType, search=graphene.String())
+    votes = graphene.List(VoteType)
 
-    def resolve_motorcycles(self, info, **kwargs):
+
+    def resolve_motorcycles(self, info, search=None, **kwargs):
+        # The value sent with the search parameter will be in the args variable
+        if search:
+            filter = (
+                Q(description__icontains=search)|
+                Q(marca__icontains=search)
+
+            )
+            return Motocicleta.objects.filter(filter)
+
         return Motocicleta.objects.all()
+
+        
+    def resolve_votes(self, info, **kwargs):
+        return Vote.objects.all()
 
 class CreateMotocicleta(graphene.Mutation):
     id = graphene.Int()
@@ -27,6 +53,8 @@ class CreateMotocicleta(graphene.Mutation):
     color = graphene.String()
     precio = graphene.Decimal()
     description = graphene.String()
+    posted_by = graphene.Field(UserType)
+
 
 
 
@@ -46,7 +74,9 @@ class CreateMotocicleta(graphene.Mutation):
 
 
     def mutate(self, info, marca, modelo, motor, consumog, capacidadg, cilindraje, version, color, precio, description):
-        motocicleta = Motocicleta(marca=marca, modelo=modelo, motor=motor, consumog=consumog, capacidadg=capacidadg, cilindraje=cilindraje, version=version, color=color, precio=precio, description=description)
+        user = info.context.user or None
+
+        motocicleta = Motocicleta(marca=marca, modelo=modelo, motor=motor, consumog=consumog, capacidadg=capacidadg, cilindraje=cilindraje, version=version, color=color, precio=precio, description=description,posted_by=user,)
         motocicleta.save()
 
         return CreateMotocicleta(
@@ -61,10 +91,38 @@ class CreateMotocicleta(graphene.Mutation):
             color=motocicleta.color,
             precio=motocicleta.precio,
             description=motocicleta.description,
+            posted_by=motocicleta.posted_by,
+
         )
+
+class CreateVote(graphene.Mutation):
+    user = graphene.Field(UserType)
+    motocicleta = graphene.Field(MotocicletaType)
+
+    class Arguments:
+        motocicleta_id = graphene.Int()
+
+    def mutate(self, info, motocicleta_id):
+        user = info.context.user
+        if user.is_anonymous:
+ #1
+            raise GraphQLError('You must be logged to vote!')
+        motocicleta = Motocicleta.objects.filter(id=motocicleta_id).first()
+        if not motocicleta:
+ #2
+            raise Exception('Invalid Link!')
+
+        Vote.objects.create(
+            user=user,
+            motocicleta=motocicleta,
+        )
+
+        return CreateVote(user=user, motocicleta=motocicleta)
 
 
 class Mutation(graphene.ObjectType):
     create_motocicleta = CreateMotocicleta.Field()
+    create_motocicleta = CreateMotocicleta.Field()
+    create_vote = CreateVote.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
