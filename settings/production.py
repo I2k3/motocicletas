f7c264b60fe61ec91b084c5e3c722d9c9f8e3832
I2k3 <itzel5kari10@gmail.com>
2023-05-30 13:34:24 -0600

    Update DEV production.py

--- settings/production.py
+++ settings/production.py
@@ -6,7 +6,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+#hola
 from pathlib import Path
 from decouple import config
 
